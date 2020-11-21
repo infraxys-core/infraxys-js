ff4424a3586c1a89369c9153a3d8d437d61d0aa8
jeroen-manders <jeroen.manders@external.mckinsey.com>
2020-11-21 14:29:23 +0100

    Initial

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  mode: 'production',
+  entry: './src/main.js',
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+};
